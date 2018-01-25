133d2bf40c2df5c43ec03e16ec62a8563cbc1af1
jscarton <jscarton@gmail.com>
2018-01-25 17:01:13 -0500

    updating recipes

--- recipes/laravel/install.sh
+++ recipes/laravel/install.sh
@@ -3,5 +3,5 @@ echo "---------------------------------------------------------------"
 echo "INSTALLING LATEST Laravel"
 echo "---------------------------------------------------------------"
 composer global require "laravel/installer"
-echo "export PATH=\$PATH:\$HOME/.composer/vendor/bin/">>/home/vagrant/.bash_profile
+echo "export PATH=\$PATH:\$HOME/.config/composer/vendor/bin/">>/home/vagrant/.bash_profile
 laravel --version
